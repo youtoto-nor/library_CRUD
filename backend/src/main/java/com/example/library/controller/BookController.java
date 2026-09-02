e3b2f479750d2af46fbb76fe79dc626ce53558b3
youtoto-nor <rebufq890@gmail.com>
2026-09-02 14:32:27 +0900

    Backend 형성
    1차 백엔드 구조 및 코드 형성

---
+++ backend/src/main/java/com/example/library/controller/BookController.java
@@ -0,0 +1,46 @@
+package com.example.library.controller;
+
+import com.example.library.dto.BookRequest;
+import com.example.library.entity.Book;
+import com.example.library.service.BookService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+    @GetMapping("/books")
+    public List<Book> getBooks() {
+        return bookService.getBooks();
+    }
+
+    @GetMapping("/books/{id}")
+    public Book getBook(@PathVariable Long id) {
+        return bookService.getBook(id);
+    }
+
+    @PostMapping("/books")
+        public Book createBook(@RequestBody BookRequest request) {
+            return bookService.createBook(request);
+    }
+
+    @PutMapping("/books/{id}")
+    public Book updateBook(
+            @PathVariable Long id,
+            @RequestBody BookRequest request) {
+
+        return bookService.updateBook(id, request);
+    }
+
+    @DeleteMapping("/books/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+    }
+}
