4f79acbc76750ed1e46bc876124cba77421eda0a
youtoto-nor <rebufq890@gmail.com>
2026-09-02 17:10:22 +0900

    프론트엔드 내부 코드 구체화
    기능 추가 및 백엔드 연결 및 프론트엔드 코드 수정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-import BookList from './features/books/BookList';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import BookList from "./features/books/BookList";
+import BookForm from "./features/books/BookForm";
+import "./App.css";
 
 function App() {
     return (
-        <div className="app">
-            <BookList />
-        </div>
+        <BrowserRouter>
+            <nav>
+                <Link to="/books">도서 목록</Link>
+                <Link to="/books/new">도서 등록</Link>
+            </nav>
+
+            <Routes>
+                <Route path="/books" element={<BookList />} />
+                <Route path="/books/new" element={<BookForm />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
-
-export default App;
+    export default App;
