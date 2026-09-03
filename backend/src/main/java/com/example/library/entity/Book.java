b6563a4f85a60ecd8b171dd2c79241eb77bfeedf
youtoto-nor <rebufq890@gmail.com>
2026-09-03 15:22:15 +0900

    Backend 내용 1차 단계로 수정 및 frontend 내용 수정

--- backend/src/main/java/com/example/library/entity/Book.java
+++ backend/src/main/java/com/example/library/entity/Book.java
@@ -1,15 +1,12 @@
 package com.example.library.entity;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "books")
 public class Book {
 
     @Id
