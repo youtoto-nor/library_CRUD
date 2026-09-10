8158f0e963cff2f78528192a896fd4c50ffe5df0
youtoto-nor <rebufq890@gmail.com>
2026-09-10 13:00:32 +0900

    도서 관리 시스템 2차 과정 보안 수정 및 css

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -14,4 +14,18 @@ api.interceptors.request.use((config) => {
     return config;
 });
 
+api.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem("token");
+            window.dispatchEvent(new Event("auth-change"));
+        }
+
+        return Promise.reject(error);
+    }
+);
+
 export default api;
